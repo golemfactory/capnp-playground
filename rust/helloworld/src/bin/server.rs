9af9222bda3bed7b63ad85b6dd308b19b0b3041e
Adam Wierzbicki <awierzbicki@golem.network>
2019-03-11 16:49:06 +0100

    TLS server

--- rust/helloworld/src/bin/server.rs
+++ rust/helloworld/src/bin/server.rs
@@ -1,10 +1,13 @@
 extern crate capnp;
 #[macro_use]
 extern crate capnp_rpc;
+extern crate native_tls;
 extern crate tokio;
+extern crate tokio_tls;
 extern crate helloworld;
 
 use std::env;
+use std::fs::read;
 use std::io::BufWriter;
 use std::net::SocketAddr;
 
@@ -16,10 +19,14 @@ use capnp_rpc::{ImbuedMessageBuilder, RpcSystem, Server};
 use capnp_rpc::rpc_twoparty_capnp::Side;
 use capnp_rpc::twoparty::VatNetwork;
 
+use native_tls::{Identity, TlsAcceptor as NativeAcceptor};
+
 use tokio::io::AsyncRead;
 use tokio::net::TcpListener;
 use tokio::prelude::{Future, Stream};
 use tokio::runtime::current_thread;
+
+use tokio_tls::TlsAcceptor as TokioAcceptor;
 
 use helloworld::{Server as SayHelloServer, SayHelloParams, SayHelloResults, ToClient};
 use helloworld::hello_reply::Builder as ReplyBuilder;
@@ -58,29 +65,47 @@ impl SayHelloServer for ServerImpl {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    // TCP listener
     let addr = args[1].parse::<SocketAddr>().unwrap();
     let listener = TcpListener::bind(&addr).unwrap();
 
+    // TLS acceptor
+    let der = read(&args[2]).unwrap();
+    let identity = Identity::from_pkcs12(der.as_slice(), "").unwrap();
+    let tls_acceptor = NativeAcceptor::builder(identity).build().unwrap();
+    let tls_acceptor = TokioAcceptor::from(tls_acceptor);
+
+    // RPC client
     let client = ToClient::new(ServerImpl::new()).into_client::<Server>();
 
     println!("Starting server...");
     current_thread::block_on_all(listener.incoming().for_each(move |stream| {
         stream.set_nodelay(true)?;
-        let (reader, writer) = stream.split();
-        let writer = BufWriter::new(writer);
+        let client = client.clone();
 
-        let network = VatNetwork::new(
-            reader,
-            writer,
-            Side::Server,
-            Default::default()
-        );
-        let rpc_system = RpcSystem::new(
-            Box::new(network),
-            Some(client.clone().client)
-        );
-        current_thread::spawn(rpc_system.map_err(|e| eprintln!("{:?}", e)));
+        let tls_accept = tls_acceptor
+            .accept(stream)
+            .and_then(move |stream| {
+                let (reader, writer) = stream.split();
+                let writer = BufWriter::new(writer);
 
+                let network = VatNetwork::new(
+                    reader,
+                    writer,
+                    Side::Server,
+                    Default::default()
+                );
+                let rpc_system = RpcSystem::new(
+                    Box::new(network),
+                    Some(client.client)
+                );
+
+                current_thread::spawn(rpc_system.map_err(|e| eprintln!("{:?}", e)));
+                Ok(())
+            });
+
+        current_thread::spawn(tls_accept.map_err(|e| eprintln!("{:?}", e)));
         Ok(())
     })).unwrap();
 }
