9e094f1e8f07983e1664af06b935530d3ebbf79d
Adam Wierzbicki <awierzbicki@golem.network>
2019-03-13 17:58:25 +0100

    Python TLS support

--- python/server.py
+++ python/server.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import socket
+import ssl
 import sys
 
 import capnp
@@ -17,11 +19,26 @@ class Server(HelloWorld.Server):
         return HelloWorld.HelloReply.new_message(message=message)
 
 
-def main(server_addr):
-    server = capnp.TwoPartyServer(server_addr, bootstrap=Server())
+def main(host, port, server_cert, server_key):
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(server_cert, server_key)
+    sock = socket.socket()
+    sock.bind((host, int(port)))
+    sock.listen()
+
     print("Starting server...")
-    server.run_forever()
+    while True:
+        conn, _ = sock.accept()
+        conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        try:
+            ssl_conn = context.wrap_socket(conn, server_side=True)
+            ssl_conn.setblocking(False)
+            server = capnp.TwoPartyServer(ssl_conn, bootstrap=Server())
+            server.on_disconnect().wait()
+            ssl_conn.close()
+        except ssl.SSLError as e:
+            print(e)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(*sys.argv[1:])
