9077830dea0f92b6f9282e6d96a607c6c3ab77e4
Adam Wierzbicki <awierzbicki@golem.network>
2019-03-13 17:58:25 +0100

    Python TLS support

--- python/client.py
+++ python/client.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import socket
+import ssl
 import sys
 
 import capnp
@@ -8,8 +10,17 @@ capnp_path = Path(__file__).parent / '..' / 'helloworld.capnp'
 HelloWorld = capnp.load(str(capnp_path)).HelloWorld
 
 
-def main(server_addr):
-    client = capnp.TwoPartyClient(server_addr)
+def main(server_host, server_port, server_cert):
+    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    context.load_verify_locations(server_cert)
+
+    sock = socket.socket()
+    sock.connect((server_host, int(server_port)))
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    conn = context.wrap_socket(sock, server_hostname="localhost")
+    conn.setblocking(False)
+
+    client = capnp.TwoPartyClient(conn)
     helloworld = client.bootstrap().cast_as(HelloWorld)
     request = HelloWorld.HelloRequest.new_message(name="World")
     print("Sending request...")
@@ -18,4 +29,4 @@ def main(server_addr):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(*sys.argv[1:])
